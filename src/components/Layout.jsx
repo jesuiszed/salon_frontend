8588d2090ed216259da0919ed5a416aa6c400155
flozed <joinz3d@gmail.com>
2025-12-19 11:03:12 +0100

    refactor(Layout): remove Header and Footer components for cleaner layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,21 +2,15 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import './Layout.css';
-import Header from './Header';
-import Footer from './Footer';
 
 const Layout = () => {
   return (
-    <>
-      <Header />
-      <div className="layout">
-        <Sidebar />
-        <div className="main-content">
-          <Outlet />
-        </div>
+    <div className="layout">
+      <Sidebar />
+      <div className="main-content">
+        <Outlet />
       </div>
-      <Footer />
-    </>
+    </div>
   );
 };
 
